NeilTheFisher/t3code

[codex] Fix onboarding connection status (#4001)
Co-authored-by: codex <codex@users.noreply.github.com>

apps/web/src/components/cloud/cloudEnvironmentConnectionPresentation.test.ts
@@ -1,0 +1,55 @@
+import type { EnvironmentConnectionPresentation } from "@t3tools/client-runtime/connection";
+import { describe, expect, it } from "vite-plus/test";
+
+import { presentSavedCloudEnvironmentConnection } from "./cloudEnvironmentConnectionPresentation";
+
+function connection(
+  phase: EnvironmentConnectionPresentation["phase"],
+  error: string | null = null,
+): EnvironmentConnectionPresentation {
+  return { phase, error, traceId: null };
+}
+
+describe("saved cloud environment connection presentation", () => {
+  it("only labels a live connection as connected", () => {
+    expect(presentSavedCloudEnvironmentConnection(connection("connected"))).toEqual({
+      buttonLabel: "Connected",
+      statusText: "Connected",
+      tone: "connected",
+    });
+
+    expect(presentSavedCloudEnvironmentConnection(connection("connecting"))).toEqual({
+      buttonLabel: "Connecting…",
+      statusText: "Connecting...",
+      tone: "connecting",
+    });
+  });
+
+  it("surfaces a failed attempt while the supervisor reconnects", () => {
+    expect(
+      presentSavedCloudEnvironmentConnection(
+        connection("reconnecting", "Relay environment endpoint is unavailable."),
+      ),
+    ).toEqual({
+      buttonLabel: "Reconnecting…",
+      statusText:
+        "Failed to connect. Reconnecting... Reason: Relay environment endpoint is unavailable.",
+      tone: "connecting",
+    });
+  });
+
+  it.each([
+    ["error", "Connection failed", "Connection failed. Reason: Access denied.", "error"],
+    ["offline", "Offline", "Offline", "idle"],
+    ["available", "Not connected", "Available", "idle"],
+  ] as const)(
+    "presents %s without claiming the environment is connected",
+    (phase, buttonLabel, statusText, tone) => {
+      expect(
+        presentSavedCloudEnvironmentConnection(
+          connection(phase, phase === "error" ? "Access denied." : null),
+        ),
+      ).toEqual({ buttonLabel, statusText, tone });
+    },
+  );
+});

apps/web/src/components/cloud/cloudEnvironmentConnectionPresentation.ts
@@ -1,0 +1,58 @@
+import {
+  connectionStatusText,
+  type EnvironmentConnectionPresentation,
+} from "@t3tools/client-runtime/connection";
+
+export interface SavedCloudEnvironmentConnectionPresentation {
+  readonly buttonLabel: string;
+  readonly statusText: string;
+  readonly tone: "connected" | "connecting" | "error" | "idle";
+}
+
+/**
+ * Present the live supervisor state for an environment that is already in the
+ * connection catalog. Catalog membership only means the environment is saved;
+ * it does not mean the connection attempt succeeded.
+ */
+export function presentSavedCloudEnvironmentConnection(
+  connection: EnvironmentConnectionPresentation,
+): SavedCloudEnvironmentConnectionPresentation {
+  switch (connection.phase) {
+    case "connected":
+      return {
+        buttonLabel: "Connected",
+        statusText: connectionStatusText(connection),
+        tone: "connected",
+      };
+    case "connecting":
+      return {
+        buttonLabel: "Connecting…",
+        statusText: connectionStatusText(connection),
+        tone: "connecting",
+      };
+    case "reconnecting":
+      return {
+        buttonLabel: "Reconnecting…",
+        statusText: connectionStatusText(connection),
+        tone: "connecting",
+      };
+    case "error":
+      return {
+        buttonLabel: "Connection failed",
+        statusText: connectionStatusText(connection),
+        tone: "error",
+      };
+    case "offline":
+      return {
+        buttonLabel: "Offline",
+        statusText: connectionStatusText(connection),
+        tone: "idle",
+      };
+    case "available":
+      return {
+        buttonLabel: "Not connected",
+        statusText: connectionStatusText(connection),
+        tone: "idle",
+      };
+  }
+}

MaTriXy/not-codex

apps/web/src/components/cloud/cloudEnvironmentConnectionPresentation.test.ts
@@ -1,0 +1,55 @@
+import type { EnvironmentConnectionPresentation } from "@notcodex/client-runtime/connection";
+import { describe, expect, it } from "vite-plus/test";
+
+import { presentSavedCloudEnvironmentConnection } from "./cloudEnvironmentConnectionPresentation";
+
+function connection(
+  phase: EnvironmentConnectionPresentation["phase"],
+  error: string | null = null,
+): EnvironmentConnectionPresentation {
+  return { phase, error, traceId: null };
+}
+
+describe("saved cloud environment connection presentation", () => {
+  it("only labels a live connection as connected", () => {
+    expect(presentSavedCloudEnvironmentConnection(connection("connected"))).toEqual({
+      buttonLabel: "Connected",
+      statusText: "Connected",
+      tone: "connected",
+    });
+
+    expect(presentSavedCloudEnvironmentConnection(connection("connecting"))).toEqual({
+      buttonLabel: "Connecting…",
+      statusText: "Connecting...",
+      tone: "connecting",
+    });
+  });
+
+  it("surfaces a failed attempt while the supervisor reconnects", () => {
+    expect(
+      presentSavedCloudEnvironmentConnection(
+        connection("reconnecting", "Relay environment endpoint is unavailable."),
+      ),
+    ).toEqual({
+      buttonLabel: "Reconnecting…",
+      statusText:
+        "Failed to connect. Reconnecting... Reason: Relay environment endpoint is unavailable.",
+      tone: "connecting",
+    });
+  });
+
+  it.each([
+    ["error", "Connection failed", "Connection failed. Reason: Access denied.", "error"],
+    ["offline", "Offline", "Offline", "idle"],
+    ["available", "Not connected", "Available", "idle"],
+  ] as const)(
+    "presents %s without claiming the environment is connected",
+    (phase, buttonLabel, statusText, tone) => {
+      expect(
+        presentSavedCloudEnvironmentConnection(
+          connection(phase, phase === "error" ? "Access denied." : null),
+        ),
+      ).toEqual({ buttonLabel, statusText, tone });
+    },
+  );
+});

apps/web/src/components/cloud/cloudEnvironmentConnectionPresentation.ts
@@ -1,0 +1,58 @@
+import {
+  connectionStatusText,
+  type EnvironmentConnectionPresentation,
+} from "@notcodex/client-runtime/connection";
+
+export interface SavedCloudEnvironmentConnectionPresentation {
+  readonly buttonLabel: string;
+  readonly statusText: string;
+  readonly tone: "connected" | "connecting" | "error" | "idle";
+}
+
+/**
+ * Present the live supervisor state for an environment that is already in the
+ * connection catalog. Catalog membership only means the environment is saved;
+ * it does not mean the connection attempt succeeded.
+ */
+export function presentSavedCloudEnvironmentConnection(
+  connection: EnvironmentConnectionPresentation,
+): SavedCloudEnvironmentConnectionPresentation {
+  switch (connection.phase) {
+    case "connected":
+      return {
+        buttonLabel: "Connected",
+        statusText: connectionStatusText(connection),
+        tone: "connected",
+      };
+    case "connecting":
+      return {
+        buttonLabel: "Connecting…",
+        statusText: connectionStatusText(connection),
+        tone: "connecting",
+      };
+    case "reconnecting":
+      return {
+        buttonLabel: "Reconnecting…",
+        statusText: connectionStatusText(connection),
+        tone: "connecting",
+      };
+    case "error":
+      return {
+        buttonLabel: "Connection failed",
+        statusText: connectionStatusText(connection),
+        tone: "error",
+      };
+    case "offline":
+      return {
+        buttonLabel: "Offline",
+        statusText: connectionStatusText(connection),
+        tone: "idle",
+      };
+    case "available":
+      return {
+        buttonLabel: "Not connected",
+        statusText: connectionStatusText(connection),
+        tone: "idle",
+      };
+  }
+}